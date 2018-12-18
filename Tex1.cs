e1b3b83547849bc45c4043d944984c7294302eee
Moonyll <s.pouwel@laposte.net>
2018-12-18 08:58:39 +0100

    Tex1

---
+++ Tex1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] jours = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+        for (int i = 0; i < jours.Length; i++)
+        Console.WriteLine(jours[i]);
+        }
+    }
+}
